fdfcc40c4e67ae7db31c91ae12efdcb7197ab37b
Yurii Fisakov <fisakov.root@gmail.com>
2018-06-26 12:15:12 +0300

    added vscode install script

---
+++ roles/desktop/files/vscode/code.sh
@@ -0,0 +1,5 @@
+code --install-extension bierner.markdown-preview-github-styles
+code --install-extension PeterJausovec.vscode-docker
+code --install-extension rupisaini.vscode-ansible-linter
+code --install-extension vscoss.vscode-ansible
+code --install-extension wholroyd.jinja
