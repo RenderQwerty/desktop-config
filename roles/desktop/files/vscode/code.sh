48e138ec18cc47f7f296eadddba70c98c9a1e372
Yurii Fisakov <fisakov.root@gmail.com>
2018-06-26 23:52:57 +0300

    removed vscode configuration: now managed with built-in plugin 'codesync'

--- roles/desktop/files/vscode/code.sh
+++
@@ -1,6 +0,0 @@
-code --install-extension bierner.markdown-preview-github-styles
-code --install-extension PeterJausovec.vscode-docker
-code --install-extension rupisaini.vscode-ansible-linter
-code --install-extension vscoss.vscode-ansible
-code --install-extension wholroyd.jinja
-code --install-extension timonwong.shellcheck
